336c6a009d4de2f8faa61835d9dd88421dbb1a88
acfiorin <acfiorin@iu.edu>
2025-09-18 16:45:09 -0400

    Correct string text

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def main():
     path_to_file = "books/frankenstein.txt"
     book_text = get_book_text(path_to_file)
     num_words = word_count(book_text)
-    print(f"{num_words} words found in document")
+    print(f"{num_words} words found in the document")
 if __name__ == "__main__":
     main()
