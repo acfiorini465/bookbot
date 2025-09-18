aa42082bc2b349fd92e6f4af6c935ab472cad37e
acfiorin <acfiorin@iu.edu>
2025-09-18 16:41:42 -0400

    Created new stats.py file and imported it

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+from stats import word_count
 def get_book_text(path_to_file): 
     with open(path_to_file) as f:
         text_as_string = f.read()
     return text_as_string
 def main():
-    path_to_file = "bookbot/books/frankenstein.txt"
+    path_to_file = "books/frankenstein.txt"
     book_text = get_book_text(path_to_file)
-    print(book_text)
+    num_words = word_count(book_text)
+    print(f"{num_words} words found in document")
 if __name__ == "__main__":
-    main
-    
-
-    
-
+    main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_count(text_as_string):
+    words = str.split(text_as_string)
+    return len(words)
