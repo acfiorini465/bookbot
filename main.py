95ad532fb36abde58b9cebcb9335904b453db82b
acfiorin <acfiorin@iu.edu>
2025-09-19 12:36:40 -0400

    Added new function to stats to create dictionary and then printed at end of main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_num
 def get_book_text(path_to_file): 
     with open(path_to_file) as f:
         text_as_string = f.read()
@@ -7,6 +7,8 @@ def main():
     path_to_file = "books/frankenstein.txt"
     book_text = get_book_text(path_to_file)
     num_words = word_count(book_text)
+    character_dict = character_num(book_text)
     print(f"{num_words} words found in the document")
+    print(character_dict)
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def word_count(text_as_string):
     words = str.split(text_as_string)
     return len(words)
+def character_num(text_as_string):
+    text = text_as_string.lower()
+    character_track = {}
+    for character in text:
+        if character.isspace() and character != " ":
+            continue
+        if character in character_track:
+            character_track[character] += 1
+        else:
+            character_track[character] = 1
+    return character_track
+            
+
+   
